4ded4e56bec2d0c399a8ab91d34c993527f4c088
GabrielRoveree <gabriel.rovere@matrixenergia.com>
2025-07-22 17:26:53 -0300

    Add Vite plugin for TypeScript declaration files and configure library build settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), dts()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'DesignSystem',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+    outDir: 'dist',
+  }
 })
